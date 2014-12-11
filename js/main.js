544df675e06730125050300e22963394a040f95e
David Veselý <davidvesely.cz@gmail.com>
2014-12-11 15:19:31 +0100

    Dropdown update

--- js/main.js
+++ js/main.js
@@ -6,15 +6,20 @@ $('.burger').on('click', function(){
 });
 
 $(document).click(function(e){
-    // Check
-    if( $(e.target).closest(".mobile-nav, .burger").length > 0 ) {
-        return false;
+    // Mobile nav
+    if(!$(e.target).closest(".mobile-nav, .burger").length > 0 ) {
+		if($('.mobile-nav').hasClass('open')){
+			$('.mobile-nav').removeClass('open');
+		}
     }
 
-    // Otherwise
-    if($('.mobile-nav').hasClass('open')){
-    	$('.mobile-nav').removeClass('open');
+    //Dropdown big
+    if(!$(e.target).closest(".header, .dropdown-big").length > 0 ) {
+		if($('.dropdown-big').hasClass('open')){
+			$('.dropdown-big').removeClass('open');
+		}
     }
+
 });
 
 $('.ul-tab a').click(function (e) {
@@ -25,6 +30,7 @@ $('.ul-tab a').click(function (e) {
 $('.dropdown-big .dropdown-toggle').on('click', function(){
 	var $dropdown = $(this).parents('.dropdown').first();
 	$dropdown.toggleClass('open');
+	$('.header').toggleClass('dropdown-open');
 });
 
 $('.search-wrapper .btn-search').on('click', function(){
